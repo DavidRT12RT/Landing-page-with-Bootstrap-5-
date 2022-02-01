e7d666dd2c25d9d6f17e00ce79cdf92a9b834a93
David Melgarejo <davidarcosm@hotmail.com>
2022-01-31 19:52:34 -0600

    Primer commit

---
+++ js/Index.js
@@ -0,0 +1,13 @@
+const sayHi = () => {
+        return 0;
+};
+
+const sayHi = () => {
+        console.log("Say hello");
+        return 0;
+};
+
+const sayHi = () => {
+        console.log("Say hello!");
+        return 0;
+};
